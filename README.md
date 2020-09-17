10237d7c5a6477dd746a9e68987580e8869c62f6
Tho Nguyen <tho.nguyen@asterist.com>
2020-09-17 16:03:38 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,60 @@
+# vue-google-sheets-signin
+
+> A simple [Vue](https://vuejs.org) directive to include  [Google Sign-In Button](https://developers.google.com/identity/sign-in/web/sign-in) behavior in any component.
+
+## Install
+
+``` bash
+$ npm install --save vue-google-sheets-signin
+
+$ yarn add vue-google-sheets-signin
+```
+## Usage
+
+Import the directive and attach it to any component.
+
+#### Example:
+
+> Important: `OnGoogleAuthSuccess` and `OnGoogleAuthFail` are mandatory methods you have to declare in your component where you are using the directive.
+
+
+``` html
+<template>
+  <button v-google-sheets-signin-button="clientId" class="google-signin-button">Continue with Google</button>
+</template>
+
+<script>
+import GoogleSheetsSignInButton from 'vue-google-sheets-signin-button-directive'
+export default {
+  directives: {
+    GoogleSheetsSignInButton
+  },
+  data: () => ({
+    clientId: 'Your_Google_Client_ID'
+  }),
+  methods: {
+    OnGoogleAuthSuccess (authData) {
+      // Receive the authenticated data (access_token, id token ...) and make your magic with the backend
+
+    },
+    OnGoogleAuthFail (error) {
+      console.log(error)
+    }
+  }
+}
+</script>
+
+<style>
+.google-signin-button {
+  color: white;
+  background-color: red;
+  height: 50px;
+  font-size: 16px;
+  border-radius: 10px;
+  padding: 10px 20px 25px 20px;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+}
+</style>
+```
+
+Happy coding.
---
+++ index.js
@@ -0,0 +1,46 @@
+import Vue from 'vue'
+
+export default Vue.directive('google-sheets-signin-button', {
+    bind: function (el, binding, vnode) {
+        CheckComponentMethods()
+        let clientId = binding.value
+        let googleSignInAPI = document.createElement('script')
+        googleSignInAPI.setAttribute('src', 'https://apis.google.com/js/api.js')
+        document.head.appendChild(googleSignInAPI)
+
+        googleSignInAPI.onload = InitGoogleButton
+
+        function InitGoogleButton() {
+            gapi.load('auth2', () => {
+                const auth2 = gapi.auth2.init({
+                    client_id: clientId,
+                    scope: 'https://www.googleapis.com/auth/drive https://www.googleapis.com/auth/spreadsheets',
+                    cookie_policy: 'single_host_origin',
+                    fetch_basic_profile: false
+                })
+                auth2.attachClickHandler(el, {},
+                    OnSuccess,
+                    OnFail
+                )
+            })
+        }
+
+        function OnSuccess(googleUser) {
+            vnode.context.OnGoogleAuthSuccess(googleUser.getAuthResponse())
+        }
+
+        function OnFail(error) {
+            vnode.context.OnGoogleAuthFail(error)
+        }
+
+        function CheckComponentMethods() {
+            if (!vnode.context.OnGoogleAuthSuccess) {
+                throw new Error('The method OnGoogleAuthSuccess must be defined on the component')
+            }
+
+            if (!vnode.context.OnGoogleAuthFail) {
+                throw new Error('The method OnGoogleAuthFail must be defined on the component')
+            }
+        }
+    }
+})
